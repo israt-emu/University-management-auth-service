629e37a7cae659824fc0dd2c0b2bf6b89e5e79c0
unknown <isratjahan.emu1998@gmail.com>
2023-06-11 21:54:02 +0600

    eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import express, {Application, Request, Response} from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 //
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 //
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is Listening..");
-});
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is Listening..')
+})
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
-const port: number = 5000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
+
+const port = config.port || 5000
 //database connection
 const db = async () => {
   try {
-    await mongoose.connect(config.databse_url as string);
-    console.log("Database connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected successfully')
     app.listen(port, () => {
-      console.log(` App listening on port ${port}`);
-    });
+      console.log(` App listening on port ${port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-};
-db();
+}
+db()
